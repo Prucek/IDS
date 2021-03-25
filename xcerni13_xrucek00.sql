32de2ff30216ccbb56dcbe0f376d5172ba445a6a
Prucek <56258989+Prucek@users.noreply.github.com>
2021-03-25 15:39:21 +0100

    Added some INSERTS

--- xcerni13_xrucek00.sql
+++ xcerni13_xrucek00.sql
@@ -9,18 +9,22 @@ DROP TABLE LivesIn CASCADE CONSTRAINT;
 DROP TABLE Ownership CASCADE CONSTRAINT;
 DROP TABLE CatOwns CASCADE CONSTRAINT;
 
+DROP SEQUENCE seq_cat;
+DROP SEQUENCE seq_race;
+DROP SEQUENCE seq_life;
+DROP SEQUENCE seq_ter;
+
 CREATE TABLE Race (
     raceID INT PRIMARY KEY,
-    color_eyes VARCHAR(100),
+    race_color_eyes VARCHAR(100),
     origin VARCHAR(100),
     max_teeth_len NUMBER(3),
     specific_features VARCHAR(100)
 );
 
 CREATE TABLE Cat (
-    catID INT PRIMARY KEY,
-    -- main_name VARCHAR(100),
-    skin VARCHAR(100),
+    catID INT PRIMARY KEY, -- skin sample
+    main_name VARCHAR(100),
     color_fur VARCHAR(100),
     color_eyes VARCHAR(100)
 );
@@ -67,8 +71,7 @@ CREATE TABLE Life(
     deathDay DATE,
     deathCause VARCHAR(100),
     bornIn INT,
-    FOREIGN KEY (bornIn) REFERENCES Teritory(teritoryID),
-    UNIQUE (bornIn)
+    FOREIGN KEY (bornIn) REFERENCES Teritory(teritoryID)
 );
 
 CREATE TABLE LivesIn(
@@ -102,19 +105,124 @@ ALTER TABLE Cat
 ADD raceFK INT;
 ALTER TABLE Cat 
 ADD FOREIGN KEY (raceFK) REFERENCES Race(raceID);
-ALTER TABLE Cat
-ADD lifeFK INT;
-ALTER TABLE Cat
-ADD FOREIGN KEY (lifeFK) REFERENCES Life(lifeID);
-ALTER TABLE Teritory
-ADD lifeFK INT;
-ALTER TABLE Teritory
-ADD FOREIGN KEY (lifeFK) REFERENCES Life(lifeID);
+
+ALTER TABLE Life
+ADD catFK INT;
+ALTER TABLE Life
+ADD FOREIGN KEY (catFK) REFERENCES Cat(catID);
+
+ALTER TABLE Life
+ADD killedIn INT;
+ALTER TABLE Life
+ADD FOREIGN KEY (killedIn) REFERENCES Teritory(teritoryID);
+
 ALTER TABLE Teritory
 ADD ownFK INT;
 ALTER TABLE Teritory
 ADD FOREIGN KEY (ownFK) REFERENCES Ownership(ownID);
+
 ALTER TABLE Host
 ADD ownFK INT;
 ALTER TABLE Host
 ADD FOREIGN KEY (ownFK) REFERENCES Ownership(ownID);
+
+
+CREATE SEQUENCE seq_race
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+INSERT INTO Race (raceID, race_color_eyes, origin, max_teeth_len, specific_features)
+    VALUES (seq_race.nextval, 'yellow', 'Turkmenistan', 3, 'Scary');
+INSERT INTO Race (raceID, race_color_eyes, origin, max_teeth_len, specific_features)
+    VALUES (seq_race.nextval, 'blue', 'Angola', 4, 'Long tail');
+INSERT INTO Race (raceID, race_color_eyes, origin, max_teeth_len, specific_features)
+    VALUES (seq_race.nextval, 'green', 'Mexico', 2, 'Sharp claws');
+INSERT INTO Race (raceID, race_color_eyes, origin, max_teeth_len, specific_features)
+    VALUES (seq_race.nextval, 'gray', 'Bratislava', 12, 'Saber-tooth ancestor');
+INSERT INTO Race (raceID, race_color_eyes, origin, max_teeth_len, specific_features)
+    VALUES (seq_race.nextval, 'green', 'Australia', 5, 'Lives in trees');
+
+CREATE SEQUENCE seq_cat
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+INSERT INTO Cat (catID, main_name, color_fur, color_eyes, raceFK)
+    VALUES (seq_cat.nextval,'Kocur','red','blue', (SELECT raceID from Race WHERE origin='Angola'));
+
+INSERT INTO Cat (catID, main_name, color_fur, color_eyes, raceFK)
+    VALUES (seq_cat.nextval,'Muf', 'gray','green', (SELECT raceID from Race WHERE origin='Mexico'));
+
+INSERT INTO Cat (catID, main_name, color_fur, color_eyes, raceFK)
+    VALUES (seq_cat.nextval,'Puf', 'black','yellow', (SELECT raceID from Race WHERE origin='Turkmenistan'));
+
+INSERT INTO Cat (catID, main_name, color_fur, color_eyes, raceFK)
+    VALUES (seq_cat.nextval,'Wuff', 'white','green', (SELECT raceID from Race WHERE origin='Mexico'));
+
+INSERT INTO Cat (catID, main_name, color_fur, color_eyes, raceFK)
+    VALUES (seq_cat.nextval,'Dunco', 'gray','yellow', (SELECT raceID from Race WHERE origin='Australia'));
+
+
+CREATE SEQUENCE seq_ter
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+INSERT INTO Teritory (teritoryID, teritoryType, capacity)-- TODO ownFK
+    VALUES (seq_ter.nextval, 'Kitchen', 2 );
+
+INSERT INTO Teritory (teritoryID, teritoryType, capacity)-- TODO ownFK
+    VALUES (seq_ter.nextval, 'Hall', 10 );
+
+INSERT INTO Teritory (teritoryID, teritoryType, capacity)-- TODO ownFK
+    VALUES (seq_ter.nextval, 'Forest', 1024 );
+
+CREATE SEQUENCE seq_life
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+INSERT INTO Life (lifeID, lifeOrder, isDead, birthDay, deathDay, deathCause, catFK, bornIn, killedIn)
+    VALUES (seq_life.nextval,
+    (SELECT Count(*) FROM (SELECT C.main_name FROM Cat C INNER JOIN Life ON Life.catFK = C.catID) WHERE main_name='Kocur') + 1,
+    'Y', DATE '2010-3-15', DATE '2010-3-16', 'Owner was unsatisfied',
+    (SELECT catID from Cat WHERE main_name='Kocur'), 
+    (SELECT teritoryID from Teritory WHERE teritoryType='Kitchen'),
+    (SELECT teritoryID from Teritory WHERE teritoryType='Kitchen'));
+
+INSERT INTO Life (lifeID, lifeOrder, isDead, birthDay, deathDay, deathCause, catFK, bornIn, killedIn)
+    VALUES (seq_life.nextval, 
+    (SELECT Count(*) FROM (SELECT C.main_name FROM Cat C INNER JOIN Life ON Life.catFK = C.catID) WHERE main_name='Kocur') + 1,
+    'N', DATE '2010-3-16', NULL, NULL, 
+    (SELECT catID from Cat WHERE main_name='Kocur'),
+    (SELECT teritoryID from Teritory WHERE teritoryType='Hall'),
+    NULL);
+
+INSERT INTO Life (lifeID, lifeOrder, isDead, birthDay, deathDay, deathCause, catFK, bornIn, killedIn)
+    VALUES (seq_life.nextval,
+    (SELECT Count(*) FROM (SELECT C.main_name FROM Cat C INNER JOIN Life ON Life.catFK = C.catID) WHERE main_name='Muf') + 1,
+    'Y', DATE '2019-8-23', DATE '2021-3-23', 'Was Old', 
+    (SELECT catID from Cat WHERE main_name='Muf'),
+    (SELECT teritoryID from Teritory WHERE teritoryType='Forest'),
+    (SELECT teritoryID from Teritory WHERE teritoryType='Hall'));
+
+INSERT INTO Life (lifeID, lifeOrder, isDead, birthDay, deathDay, deathCause, catFK, bornIn, killedIn)
+    VALUES (seq_life.nextval,
+    (SELECT Count(*) FROM (SELECT C.main_name FROM Cat C INNER JOIN Life ON Life.catFK = C.catID) WHERE main_name='Dunco') + 1,
+    'N', DATE '1952-6-24', NULL, NULL, 
+    (SELECT catID from Cat WHERE main_name='Dunco'),
+    (SELECT teritoryID from Teritory WHERE teritoryType='Kitchen'),
+    NULL);
+
+
+
+
+SELECT * FROM Cat;
+SELECT * FROM Race;
+SELECT * FROM Life;
+SELECT * FROM Teritory;
+SELECT * FROM LivesIn;
+SELECT * FROM Ownership;
+
+-- SELECT * FROM Cat C INNER JOIN Race ON C.raceFK = Race.raceID;
